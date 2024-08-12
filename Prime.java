3d03f835ea41cd329e694835301afbdffe83caf8
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-08-12 19:29:39 +0700

    update

--- Prime.java
+++ Prime.java
@@ -4,28 +4,23 @@ public class Prime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inp = scanner.nextInt();
-        int current = inp;
         
-        int sqru = (int)Math.sqrt(inp);
-        boolean isPrime = true;
-        for(int i = 2; i <= sqru; i++)
-        {
-
-            System.out.println(inp +" % "+ i);
-            int test = inp % i; 
-            System.out.println("This is the number after dividing - "+test);
-            if(test == 0){
-                System.out.println("EFFFFFFFFFf");
-                isPrime = false;
-                break;
-                
-            }        
-        }
-        if(isPrime == true)
-        {
-            System.out.println("The number " + current + " is Prime.");
-        }else{
-            System.out.println("not Prime");
+        if (inp == 1) {
+            System.out.println("No");
+        } else {
+            int sqru = (int)Math.sqrt(inp);
+            boolean isPrime = true;
+            for(int i = 2; i <= sqru; i++) {
+                if(inp % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
         }
         scanner.close();
     }
---
+++ RecButMoreCus.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * RecButMoreCus
+ */
+public class RecButMoreCus {
+
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] parts = input.split(" ");
+
+        int firstValue = Integer.parseInt(parts[0]);
+        int secondValue = Integer.parseInt(parts[1]);
+        char symbol = '*';
+        String line = "";
+        for(int i = 0; i < secondValue; i++){
+            line += symbol;
+        }
+        for(int i = 0;i < firstValue; i++){
+            System.out.println(line);
+        }
+    }
+}
