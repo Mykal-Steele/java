582ad2ef00e4776130a4f7e56b6ef7f484f41e60
Mykal-Steele <mykal.stele@gmail.com>
2024-09-23 15:32:18 +0700

    file upload

---
+++ Mountain.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Mountain {
+    public static void main(String[] args) {
+       
+        int[] occurrences = new int[101];  
+        
+    
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            int number = scanner.nextInt();
+            
+         
+            if (number == 0) {
+                break;
+            }
+            if (number >= 1 && number <= 100) {
+                occurrences[number]++;
+            }
+        }
+        
+
+        for (int i = 1; i < occurrences.length; i++) {
+            if (occurrences[i] > 0) {
+                System.out.println(i + " occurs " + occurrences[i] + " " + (occurrences[i] > 1 ? "times" : "time"));
+            }
+        }
+    }
+}
