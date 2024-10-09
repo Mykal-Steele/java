ae00de734d23b41d47b4a59cf156e210cff3d3cd
Mykal-Steele <mykal.stele@gmail.com>
2024-10-09 09:38:10 +0700

    a

--- OOPReuse/OOpyoutube/Main.java
+++ OOPReuse/OOpyoutube/Main.java
@@ -2,9 +2,7 @@ package OOPReuse.OOpyoutube;
 
 public class Main {
     public static void main(String[] args) {
-        User us = new User();
-        us.name = "user1";
-        us.age = 12;
+        
     }
     
 
