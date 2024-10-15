4f409100106396f96fac29c2f5941c75ecbbccfb
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-10-15 21:26:07 +0700

    linearapproximation

---
+++ codingProblems/LinearApproximation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LinearApproximation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter a value for x: ");
+        double x = scanner.nextDouble();
+        
+        
+        double a = Math.ceil(x);
+        
+        
+        while (Math.sqrt(a) % 1 != 0) {
+            a++;
+        }
+       
+        double fa = Math.sqrt(a);
+        double derivativeFa = 1 / (2 * Math.sqrt(a));
+        double Lx = fa + derivativeFa * (x - a);   
+        System.out.println("The linear approximation of Square Root of " + x + " is: " + Lx);
+
+    }
+}
