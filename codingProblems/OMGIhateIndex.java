b6a1c8736575050e2db042d6659f0b87f75afbf1
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-10-28 03:21:52 +0700

    fixed names

--- codingProblems/OMGIhateIndex.java
+++ codingProblems/OMGIhateIndex.java
@@ -1,8 +1,43 @@
-
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class OMGIhateIndex {
-
     public static void main(String[] args) {
-        System.out.println("smt\ne");
+        Scanner scanner = new Scanner(System.in);
+
+        int fSize = scanner.nextInt();
+        int[] firstSet = new int[fSize];
+
+        for (int i = 0; i < fSize; i++) {
+            firstSet[i] = scanner.nextInt();
+        }
+
+        int sSize = scanner.nextInt();
+        int[] secondSet = new int[sSize];
+
+        for (int i = 0; i < sSize; i++) {
+            secondSet[i] = scanner.nextInt();
+        }
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for (int x : firstSet) {
+            hash.put(x, 1);
+        }
+        for (int x : secondSet) {
+            hash.put(x, hash.getOrDefault(x, 0) + 1);
+        }
+        String out = "";
+        for (int key : hash.keySet()) {
+            if (hash.get(key) > 1) {
+                out += key + " ";
+            }
+        }
+
+        if (out.length() > 2) {
+            System.out.print(out);
+        } else {
+            System.out.print("No common elements");
+        }
+        int counter = 0;
+
     }
 }
