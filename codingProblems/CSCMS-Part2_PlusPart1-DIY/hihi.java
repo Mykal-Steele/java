d4056a18e11079e366f8bf0bd3ac9580a1630086
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-10-31 10:16:40 +0700

    idk

---
+++ codingProblems/CSCMS-Part2_PlusPart1-DIY/hihi.java
@@ -0,0 +1,36 @@
+import java.time.LocalTime;
+public class hihi{
+    public static void main(String[] args) {
+        
+        
+        String x = "";
+        char[] targer = {'H', 'e','l','l','o', '_', 'W','o','r','l','d', '!'};
+        char cur = '0';
+        int index = 0;
+        
+        for(int i = 0; i <targer.length; i++){
+            
+            while(cur <= targer[index]){
+                if(cur == targer[i]){
+                    break;
+                }
+                if(cur++ !=)
+                cur++;
+                System.out.println(x + (char) cur);
+                
+                try{
+                    Thread.sleep(5);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                
+                
+            }
+            x+=cur;
+            cur = '0';
+            index++;
+        }
+        
+       
+    }
+}
