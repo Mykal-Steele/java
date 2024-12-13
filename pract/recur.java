2270bdc423105c7911615fba7896da8dfc95964c
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2024-12-13 14:38:04 +0700

    excel

--- pract/recur.java
+++ pract/recur.java
@@ -2,16 +2,20 @@ public class recur{
     public static void main(String[] args){
         int x= 0;
         int y= 2;
-        int z = Gcd(x,y);
+        int z = Gcd(5);
+        System.out.println(z);
+
 
 
 
     }
-    private static int Gcd(int x,int y){
+    private static int Gcd(int x){
 
-        if(x%y == 0) return y;
-        else
-            return Gcd(y,x&y);
+    if(x == 1){
+        return 1;
+    }
+    return x* Gcd(x - 1);
+
     }
     
 }
